c119564a477c882e06c79578f4dd0bf99d66959e
oBrenow <brreact@gmail.com>
2025-07-28 20:00:24 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,73 @@
+     // Configuração do ScrollReveal
+        ScrollReveal({ reset: true });
+        
+        // Animação do cabeçalho
+        ScrollReveal().reveal('header', { 
+            delay: 100,
+            duration: 1000,
+            origin: 'top',
+            distance: '50px'
+        });
+        
+        // Animação do topo do site
+        ScrollReveal().reveal('.txt-topo', { 
+            delay: 100,
+            duration: 1000,
+            origin: 'left',
+            distance: '50px'
+        });
+        
+        ScrollReveal().reveal('.img-topo', { 
+            delay: 100,
+            duration: 1000,
+            origin: 'right',
+            distance: '50px'
+        });
+        
+        // Animação das especialidades
+        ScrollReveal().reveal('.titulo', { 
+            delay: 100,
+            duration: 800,
+            origin: 'top',
+            distance: '30px'
+        });
+        
+        ScrollReveal().reveal('.especialidades-box', { 
+            delay: 100,
+            duration: 1000,
+            interval: 200,
+            origin: 'bottom',
+            distance: '50px'
+        });
+        
+        // Animação da seção sobre
+        ScrollReveal().reveal('.txt-sobre', { 
+            delay: 100,
+            duration: 1000,
+            origin: 'left',
+            distance: '50px'
+        });
+        
+        ScrollReveal().reveal('.txt-icone', { 
+            delay: 100,
+            duration: 1000,
+            origin: 'right',
+            distance: '50px'
+        });
+        
+        // Animação dos projetos
+        ScrollReveal().reveal('.img-port', { 
+            delay: 100,
+            duration: 1000,
+            interval: 200,
+            origin: 'bottom',
+            distance: '50px'
+        });
+
+
+          ScrollReveal().reveal('form', { 
+            delay: 100,
+            duration: 2000,
+            origin: 'right',
+            distance: '50px'
+        });
